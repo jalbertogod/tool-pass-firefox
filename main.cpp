cf48c384b052c799cbb0bc16fe8cea32951d17c9
jalbertogod <jalbertogod@gmail.com>
2021-10-29 22:09:33 -0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "tools-pass-fox.hpp"
-int main(int argc,char ** argv){
-    ToolsPassFox tpf;
-    tpf.filepass="NovaSenha";
-    std::cout << (tpf.export_file(tpf.filepass)?"Exportado com sucesso!":"Falha ao exportar!") << std::endl;
-   tpf.export_file("senha");
-    return 0; 
-
-}
